d660eac4ade809a037ceb9ece5a275cfcd26d84b
Andreseag <castrogomezand@gmail.com>
2022-09-07 17:05:01 -0500

    build(core): Add react router and create principal routes

---
+++ src/pages/Character/Character.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export interface CharacterInterface {}
+
+const Character: React.FC<CharacterInterface> = () => {
+	return <div >Character</div>;
+};
+
+export default Character;
+
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export interface HomeInterface {}
+
+const Home: React.FC<HomeInterface> = () => {
+	return <div ><h1>HOME</h1></div>;
+};
+
+export default Home;
+
